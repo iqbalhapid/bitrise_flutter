231e8027af1f7bff6296bc6e97bffacc7dc4622c
Brian Esa Chrisnando <brian.esa@dkatalis.com>
2020-01-21 14:24:34 +0700

    added yes

--- lib/main.dart
+++ lib/main.dart
@@ -97,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter $myapi',
+              '$_counter $myapi yes',
               style: Theme.of(context).textTheme.display1,
             ),
           ],
