f32580101a16c0b1b155bc13c958bcb16aad160f
iqbalhapid_Dkatalis <iqbal.hafid@dkatalis.com>
2020-01-22 17:02:13 +0700

    adding validator on register page

--- lib/screen/auth/loginPage.dart
+++ lib/screen/auth/loginPage.dart
@@ -16,15 +16,13 @@ class _LoginPageState extends State<LoginPage> {
 
   final globalWidget = new GlobalWidget();
 
-  Widget _bodyLogin(){
+  Widget _bodyLogin() {
     return Container(
-    child:
-    Scaffold(
+      child: Scaffold(
         backgroundColor: Colors.black45,
         body: Container(
           width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20)),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
           padding: EdgeInsets.all(45),
           child: ListView(
             children: <Widget>[
@@ -37,42 +35,44 @@ class _LoginPageState extends State<LoginPage> {
                     Padding(
                       padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
                       child: TextFormField(
-                        validator: RequiredValidator(errorText: 'Required'),
-                        onChanged: (text) => _email = text,
-                        style: TextStyle(color: Colors.white),
-                        decoration: globalWidget.inputDecoration(Colors.white, Colors.white, 'Email', 15)
-                      ),
+                          validator: RequiredValidator(errorText: 'Required'),
+                          onChanged: (text) => _email = text,
+                          style: TextStyle(color: Colors.white),
+                          decoration: globalWidget.inputDecoration(
+                              Colors.white, Colors.white, 'Email', 15)),
                     ),
+                    
                     Padding(
                       padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
                       child: TextFormField(
-                        validator: RequiredValidator(errorText: 'Required'),
-                        obscureText: true,
-                        onChanged: (text) => _password = text,
-                        style: TextStyle(color: Colors.white),
-                        decoration: globalWidget.inputDecoration(Colors.white, Colors.white, 'Password', 15)
-                      ),
+                          validator: RequiredValidator(errorText: 'Required'),
+                          obscureText: true,
+                          onChanged: (text) => _password = text,
+                          style: TextStyle(color: Colors.white),
+                          decoration: globalWidget.inputDecoration(
+                              Colors.white, Colors.white, 'Password', 15)),
                     ),
+                   
                   ],
                 ),
               ),
               Padding(
-                padding: EdgeInsets.only(top: 20),
-                child: globalWidget.actionButton(context, 'LOGIN', validateInputs, Colors.lightGreen)
-              ),
+                  padding: EdgeInsets.only(top: 20),
+                  child: globalWidget.actionButton(
+                      context, 'LOGIN', validateInputs, Colors.lightGreen)),
               SizedBox(height: 20.0),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text("Don't have an account? "),
                   GestureDetector(
-                    onTap: () {
+                      onTap: () {
                         Navigator.pushNamed(context, '/registerPage');
-                    },
-                    child: Text('Register',
-                      style: TextStyle(color: Colors.greenAccent, fontWeight: FontWeight.bold)
-                    )
-                  )
+                      },
+                      child: Text('Register',
+                          style: TextStyle(
+                              color: Colors.greenAccent,
+                              fontWeight: FontWeight.bold)))
                 ],
               ),
               //_tryToLogin(model.appState)
@@ -96,12 +96,9 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: globalWidget.background(Colors.greenAccent, Colors.grey, Colors.white, Colors.lime),
-        child: 
-            _bodyLogin()
-        
-        )
-    );
+        body: Container(
+            decoration: globalWidget.background(
+                Colors.greenAccent, Colors.grey, Colors.white, Colors.lime),
+            child: _bodyLogin()));
   }
 }
